e1a1c89bc13f689103b0b0a9fe9794dbc09cb9d5
Joaquín Miguez <joacodeviaje@gmail.com>
2013-08-05 17:06:47 -0300

    template caching

--- app/Sarasa/Models/Template.php
+++ app/Sarasa/Models/Template.php
@@ -10,4 +10,13 @@ class Template extends \Sarasa\Core\Template
 
         # Declare plugins for template if necessary
     }
+
+    public function cache()
+    {
+        if (\Sarasa\Core\FrontController::config('production')) {
+            $this->setCaching(parent::CACHING_LIFETIME_SAVED);
+            $this->setCacheLifetime(\Sarasa\Core\FrontController::config('cache_lifetime'));
+            $this->setCompileCheck(false);
+        }
+    }
 }
--- cli-config.php
+++ cli-config.php
@@ -1,29 +1,30 @@
 <?php
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-// replace with file to your own project bootstrap
 require_once "vendor/autoload.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Sarasa\Core\Config;
 
-$paths = array("models");
+$paths = array("app/");
 $isDevMode = false;
 
 $string = file_get_contents("config.json");
 $settings = json_decode($string, true);
 
 $dbParams = array(
-		'driver'   => 'pdo_mysql',
-		'user'     => $settings['dbuser'],
-		'password' => $settings['dbpass'],
-		'dbname'   => $settings['dbname'],
-	);
-	
+        'driver'   => 'pdo_mysql',
+        'user'     => $settings['dbuser'],
+        'password' => $settings['dbpass'],
+        'dbname'   => $settings['dbname'],
+    );
+    
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
 
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
-));
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(
+    array(
+        'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
+    )
+);
